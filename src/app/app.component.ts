e2abb106720ff280bd33d88d1292a21da8ccd51d
Thalisson Almeida <134632200+almeidatl@users.noreply.github.com>
2024-11-29 17:06:19 -0300

    add: sobre

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './core/components/header/header.component';
 import { HeroComponent } from './core/components/hero/hero.component';
+import { SobreComponent } from "./core/components/sobre/sobre.component";
 
 @Component({
   selector: 'app-root', 
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, HeroComponent],
+  imports: [RouterOutlet, HeaderComponent, HeroComponent, SobreComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
