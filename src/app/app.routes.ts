1a3d137c6a2e190b19367036964f32b794065181
Thalisson Almeida <134632200+almeidatl@users.noreply.github.com>
2024-12-02 15:56:00 -0300

    feat: cria pages principais

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { listenerCount } from 'node:events';
+import { HeaderComponent } from './core/components/header/header.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HeaderComponent}
+];
